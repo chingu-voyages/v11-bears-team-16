375bd52c2a914fa6aabd65efa74804bbb11e8e2c
willjw3 <will.j.ward3@gmail.com>
2019-09-08 13:53:32 +0900

    Added gatsby-config file and google fonts package

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    plugins: [
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+              fonts: [
+                `Raleway`,
+                `Orbitron`,
+                `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+              ]
+            }
+          },
+    ]
+}
