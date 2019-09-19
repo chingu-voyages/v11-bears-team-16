4a1e7fac12f5937c9954a737fe6734609d8e43ca
Michael <tranmichael97@gmail.com>
2019-09-19 12:30:49 +1000

    Add link for bubble sort

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,6 @@ import React, { useState, Fragment } from "react";
 import Layout from "../components/layout";
 import { Link } from "gatsby";
 
-import BinarySearch from "../components/algorithms/BinarySearch";
-
 const Index = () => {
   const sortedList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -11,6 +9,7 @@ const Index = () => {
     <Layout>
       <p style={{ padding: `20px 20px` }}>This is the home page</p>
       <Link to="/binary-search">Binary Search</Link>
+      <Link to="/bubble-sort">Bubble Sort</Link>
     </Layout>
   );
 };
